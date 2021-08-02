7e85c4d0b4b2ab1434dae755e0c63eb2203f36eb
Kirk <kalitrek@protonmail.com>
2021-08-01 19:59:46 -0500

    Initial Commit

---
+++ Principles of Programming with C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Principles of Programming with C++
+//
+//  Created by Kirk Everett Teal Jr. on 8/1/21.
+//  Copyright © 2021 Kirk Everett Teal Jr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
